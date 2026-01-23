70e8797772ac56a86d2ebb0c3dd3b0287993b37c
Louis Maddox <louismmx@gmail.com>
2026-01-23 12:41:05 +0000

    docs: document benchmark results

--- benchmark/run_large_benchmark.sh
+++ benchmark/run_large_benchmark.sh
@@ -1,6 +1,6 @@
 (
-    cd awesome_python_readme_x1000 && \
-    # Download a large README file for benchmarking - ~7MB
+    cd pivotpy_readme_x1000 && \
+    # Download a large README file for benchmarking (~7MB)
     curl -sOL https://github.com/asaboor-gh/pivotpy/blob/2ad43748d0a306412f1c9ee889ff3647f4f196c8/README.md && \
     hyperfine \
         'python comrak_bench.py' -n baseline \
