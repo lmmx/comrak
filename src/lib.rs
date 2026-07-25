ff2f32e664c2134892afde9b7fe800f4099ff49a
Louis Maddox <lmmx@users.noreply.github.com>
2026-07-26 00:09:12 +0100

    more opts (#82)
    * fix: cover all fields
    
    * docs: some more user relevant info in readme
    
    * chore(pre-commit): autofix run
    
    * chore: lint
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use comrak_lib::{markdown_to_html, Options as ComrakOptions};
 
 // Import the Python option classes we defined
 mod options;
-use options::{PyAlertStyle, PyExtensionOptions, PyParseOptions, PyRenderOptions};
+use options::{PyAlertStyle, PyExtensionOptions, PyListStyle, PyParseOptions, PyRenderOptions};
 
 /// Render a Markdown string to HTML, with optional Extension/Parse/Render overrides.
 #[pyfunction(signature=(text, extension_options=None, parse_options=None, render_options=None))]
@@ -43,6 +43,8 @@ mod comrak {
     use super::PyAlertStyle;
     #[pymodule_export]
     use super::PyExtensionOptions;
+    #[pymodule_export]
+    use super::PyListStyle;
     #[pymodule_export]
     use super::PyParseOptions;
     #[pymodule_export]
